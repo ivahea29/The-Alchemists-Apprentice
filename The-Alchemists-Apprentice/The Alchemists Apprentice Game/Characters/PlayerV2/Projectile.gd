9ce7731b85e4fc83a2eff3211930b4815effaf07
georgiazrk <gzrkearns@gmail.com>
2023-06-12 16:35:04 +1200

    Player version 2: Updated with projectile attack and animations

---
+++ The-Alchemists-Apprentice/The Alchemists Apprentice Game/Characters/PlayerV2/Projectile.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+class_name Projectile
+
+
+var speed: float = 500
+var direction = 1
+var velocity: Vector2 = Vector2()
+const GRAVITY = 200
+
+func _physics_process(delta: float):
+	velocity.y += GRAVITY * delta
+	velocity.x = direction * speed
+	position += velocity * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
