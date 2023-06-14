edd849dddf855d42551f6367ca49d3d0b336a525
georgiazrk <gzrkearns@gmail.com>
2023-06-14 13:26:11 +1200

    Merge branch 'main' of https://github.com/ivahea29/The-Alchemists-Apprentice
    # Conflicts:
    #	The-Alchemists-Apprentice/The Alchemists Apprentice Game/Characters/PlayerV2/player_v_2.tscn
    #	The-Alchemists-Apprentice/The Alchemists Apprentice Game/Scenes/Levels/level_1.tscn
    #	The-Alchemists-Apprentice/The Alchemists Apprentice Game/Scenes/Levels/level_2.tscn

--- The-Alchemists-Apprentice/The Alchemists Apprentice Game/GlobalScripts/global_script.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var player_current_attack = false
-
-func some_function():
-	pass
-	
--- The-Alchemists-Apprentice/The Alchemists Apprentice Game/Scenes/Levels/level_2.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-
-func _on_area_2d_body_entered(body):
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_2.tscn")
