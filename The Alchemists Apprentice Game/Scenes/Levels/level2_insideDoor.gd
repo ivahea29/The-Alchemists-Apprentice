8eb23bdca6e3aaf8d423cafc951ea1302a2ad6b3
georgiazrk <gzrkearns@gmail.com>
2023-06-14 17:26:45 +1200

    changes to level 2 for real, Ivans was level 3 silly pants

--- The Alchemists Apprentice Game/Scenes/Levels/level2_insideDoor.gd
+++ The Alchemists Apprentice Game/Scenes/Levels/level2_insideDoor.gd
@@ -5,17 +5,20 @@ extends Area2D
 func _ready():
 	pass # Replace with function body.
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
 	if event.is_action_pressed("interact"):
-		if get_overlapping_bodies().size() > 1:
-			var playerOutside = get_parent().get_node("PlayerV2")
-			playerOutside.position = Vector2(8862, 400)
+		if get_overlapping_bodies().size() > 0:
+			var playerInside = get_parent().get_node("PlayerV2")
+			playerInside.position = Vector2(7186, 554)
+
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
-		$outsideOpenDoor.show()
+		$insideOpenDoor.show()
 
 
 func _on_body_exited(body):
 	if get_overlapping_bodies().size() < 2:
-		$outsideOpenDoor.hide()
+		$insideOpenDoor.hide()
--- The Alchemists Apprentice Game/Scenes/Levels/level_2.gd
+++ The Alchemists Apprentice Game/Scenes/Levels/level_2.gd
@@ -4,7 +4,10 @@ extends Node2D
 
 func _on_area_2d_body_entered(body):
 	if body is CharacterBody2D:
-		$PlayerV2.position = Vector2(3248, 576)
+		if body.position.x < 3616:
+			$PlayerV2.position = Vector2(3248, 576)
+		else:
+			$PlayerV2.position = Vector2(3923, 592)
 
 
 func _on_pausemenu_resume_game():
