30cf046f9b18f206a953f896c96b46c5d42c9a43
georgiazrk <gzrkearns@gmail.com>
2023-06-15 21:54:16 +1200

    added speech bubbles to level 1, added speech bubble assets important and standard, 2 of each for animation purposes. Animation in level 1

--- The Alchemists Apprentice Game/Scenes/Levels/level_1.gd
+++ The Alchemists Apprentice Game/Scenes/Levels/level_1.gd
@@ -1,2 +1,8 @@
 extends Node2D
 
+
+func _on_scroll_scroll_collected():
+	$scrollTimer.start()
+	
+func _on_scroll_timer_timeout():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_2.tscn")
