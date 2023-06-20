d781a70d80ca0d7cc3e954c6561cc265bf3a7bf9
ivahea29 <102086981+ivahea29@users.noreply.github.com>
2023-06-21 11:31:19 +1200

    changes to pause menu

--- The Alchemists Apprentice Game/Scenes/Menu/pause_menu.gd
+++ The Alchemists Apprentice Game/Scenes/Menu/pause_menu.gd
@@ -1,16 +1,5 @@
-extends Control
+extends CanvasLayer
 
-@export var game_manager : GameManager
- 
-func _ready():
-	hide()
-	game_manager.connect("toggle_game_paused", _on_game_manager_toggle_game_paused)
-	
-func _process(delta):
-	pass
 
-func _on_game_manager_toggle_game_paused(is_paused : bool):
-	if(is_paused):
-		show()
-	else:
-		hide()
+
+
