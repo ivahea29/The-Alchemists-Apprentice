777932f6efa434b12bc31ed13ed94264cc8edd8a
ivahea29 <102086981+ivahea29@users.noreply.github.com>
2023-06-21 19:00:40 +1200

    .

--- The Alchemists Apprentice Game/Scenes/Menu/main_menu.gd
+++ The Alchemists Apprentice Game/Scenes/Menu/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+	SceneTransition.change_scene_to_file("res://Scenes/StartScenes/intro_scene.tscn")
 
 
 func _on_how_to_play_pressed():
@@ -14,7 +14,7 @@ func _on_controls_pressed():
 
 
 func _on_about_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Menu/about_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu/AUthors.tscn")
 
 
 func _on_quit_pressed():
