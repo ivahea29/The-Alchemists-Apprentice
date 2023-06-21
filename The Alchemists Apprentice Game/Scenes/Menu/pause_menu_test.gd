cf0c19073d83c552d66feae0848547f8aa864ff7
ivahea29 <102086981+ivahea29@users.noreply.github.com>
2023-06-21 12:26:02 +1200

    music

--- The Alchemists Apprentice Game/Scenes/Menu/pause_menu_test.gd
+++ The Alchemists Apprentice Game/Scenes/Menu/pause_menu_test.gd
@@ -3,24 +3,19 @@ extends CanvasLayer
 @onready var pausemenucontrol = $pausemenucontrol
 
 func _ready():
-	pausemenucontrol.visible = false
-	
+	pass 
 func _process(delta):
-	
+
 	if Input.is_action_just_pressed("ui_cancel"):
-		if(pausemenucontrol.visible == false):
-			pausemenucontrol.visible = true
-		elif(pausemenucontrol.visible == true):
-			pausemenucontrol.visible = false
+		pausemenucontrol.visible = true
+		get_tree().paused = true
 
 func _on_resumebtn_pressed():
 	pausemenucontrol.visible = false
 	get_tree().paused = false
 
+func _on_quitbtn_pressed():
+	get_tree().quit()
 
 func _on_main_menubtn_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
-
-
-func _on_quitbtn_pressed():
-	get_tree().quit()
