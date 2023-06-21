f346946637d559e03b2590d315c5ee2908ce4dec
ivahea29 <102086981+ivahea29@users.noreply.github.com>
2023-06-21 14:48:41 +1200

    added enemies, gems, herb guy stuff

---
+++ The Alchemists Apprentice Game/Dialogue/herb_guy.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "HermanD"
+
+func action() -> void:
+	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+
+
+
