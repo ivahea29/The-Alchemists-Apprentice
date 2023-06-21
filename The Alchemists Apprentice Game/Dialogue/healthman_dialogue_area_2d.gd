eefa8af5db1393eaab747aaf64c72c03c5bc51c0
ivahea29 <102086981+ivahea29@users.noreply.github.com>
2023-06-21 13:57:21 +1200

    music files

---
+++ The Alchemists Apprentice Game/Dialogue/healthman_dialogue_area_2d.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "HealthmanD"
+
+func action() -> void:
+	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+
+
+
