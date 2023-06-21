91a1e468df414ad0941c709f982db3ccef7ae460
georgiazrk <gzrkearns@gmail.com>
2023-06-21 12:25:16 +1200

    pause menu attempted fix. full functionality except it doesnt actually pause the game lol

---
+++ The Alchemists Apprentice Game/Scenes/Menu/pause_menu_test.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+@onready var pausemenucontrol = $pausemenucontrol
+
+func _ready():
+	pausemenucontrol.visible = false
+	
+func _process(delta):
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		if(pausemenucontrol.visible == false):
+			pausemenucontrol.visible = true
+		elif(pausemenucontrol.visible == true):
+			pausemenucontrol.visible = false
+
+func _on_resumebtn_pressed():
+	pausemenucontrol.visible = false
+	get_tree().paused = false
+
+
+func _on_main_menubtn_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
+
+
+func _on_quitbtn_pressed():
+	get_tree().quit()
